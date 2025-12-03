5039e0c4758481c92da4ef0ede936802e5964a85
Joseth Diaz <josethd@tecnologicoismac.edu.ec>
2025-12-02 23:09:36 -0500

    Integracion Proyecto

---
+++ src/main/java/com/distribuida/ventas_spring/VentasSpringApplication.java
@@ -0,0 +1,15 @@
+package com.distribuida.ventas_spring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class VentasSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VentasSpringApplication.class, args);
+	}
+
+
+
+
+
+
+}
