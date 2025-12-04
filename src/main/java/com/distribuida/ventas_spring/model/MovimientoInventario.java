cc152a71e636a27ee01c677c7c92f85da27575b5
carlosvega87 <cvega@tecnologicoismac.edu.ec>
2025-12-03 23:51:26 -0500

    movinventario

--- src/main/java/com/distribuida/ventas_spring/model/MovimientoInventario.java
+++ src/main/java/com/distribuida/ventas_spring/model/MovimientoInventario.java
@@ -4,83 +4,80 @@ public class MovimientoInventario {
 
 
         private int idMovimiento;
-        private int idProducto;
         private String tipoMovimiento;
         private int cantidad;
         private String fechaMovimiento;
         private String descripcion;
 
-        public MovimientoInventario() {}
+        private Pedido pedido;
 
-        public MovimientoInventario(int idMovimiento, int idProducto, String tipoMovimiento,
-                                    int cantidad, String fechaMovimiento, String descripcion) {
-            this.idMovimiento = idMovimiento;
-            this.idProducto = idProducto;
-            this.tipoMovimiento = tipoMovimiento;
-            this.cantidad = cantidad;
-            this.fechaMovimiento = fechaMovimiento;
-            this.descripcion = descripcion;
-        }
-
-        // Getters y Setters
-        public int getIdMovimiento() {
-            return idMovimiento;
-        }
-
-        public void setIdMovimiento(int idMovimiento) {
-            this.idMovimiento = idMovimiento;
-        }
-
-        public int getIdProducto() {
-            return idProducto;
-        }
-
-        public void setIdProducto(int idProducto) {
-            this.idProducto = idProducto;
-        }
-
-        public String getTipoMovimiento() {
-            return tipoMovimiento;
-        }
-
-        public void setTipoMovimiento(String tipoMovimiento) {
-            this.tipoMovimiento = tipoMovimiento;
-        }
-
-        public int getCantidad() {
-            return cantidad;
-        }
-
-        public void setCantidad(int cantidad) {
-            this.cantidad = cantidad;
-        }
-
-        public String getFechaMovimiento() {
-            return fechaMovimiento;
-        }
-
-        public void setFechaMovimiento(String fechaMovimiento) {
-            this.fechaMovimiento = fechaMovimiento;
-        }
-
-        public String getDescripcion() {
-            return descripcion;
-        }
-
-        public void setDescripcion(String descripcion) {
-            this.descripcion = descripcion;
-        }
-
-        @Override
-        public String toString() {
-            return "MovimientoInventario{" +
-                    "idMovimiento=" + idMovimiento +
-                    ", idProducto=" + idProducto +
-                    ", tipoMovimiento='" + tipoMovimiento + '\'' +
-                    ", cantidad=" + cantidad +
-                    ", fechaMovimiento='" + fechaMovimiento + '\'' +
-                    ", descripcion='" + descripcion + '\'' +
-                    '}';
-        }
+    public MovimientoInventario(int idMovimiento, String tipoMovimiento, int cantidad, String fechaMovimiento, String descripcion, Pedido pedido) {
+        this.idMovimiento = idMovimiento;
+        this.tipoMovimiento = tipoMovimiento;
+        this.cantidad = cantidad;
+        this.fechaMovimiento = fechaMovimiento;
+        this.descripcion = descripcion;
+        this.pedido = pedido;
     }
+
+    public int getIdMovimiento() {
+        return idMovimiento;
+    }
+
+    public void setIdMovimiento(int idMovimiento) {
+        this.idMovimiento = idMovimiento;
+    }
+
+    public String getTipoMovimiento() {
+        return tipoMovimiento;
+    }
+
+    public void setTipoMovimiento(String tipoMovimiento) {
+        this.tipoMovimiento = tipoMovimiento;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getFechaMovimiento() {
+        return fechaMovimiento;
+    }
+
+    public void setFechaMovimiento(String fechaMovimiento) {
+        this.fechaMovimiento = fechaMovimiento;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public String toString() {
+        return "MovimientoInventario{" +
+                "idMovimiento=" + idMovimiento +
+                ", tipoMovimiento='" + tipoMovimiento + '\'' +
+                ", cantidad=" + cantidad +
+                ", fechaMovimiento='" + fechaMovimiento + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", pedido=" + pedido +
+                '}';
+    }
+}
 
